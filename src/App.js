33c57e5e996418c3c22ea3885002a096c5ee651a
xujianhua <xujianhuaap@gmail.com>
2017-11-27 23:16:50 +0800

    【develop】Route exact　prop

--- src/App.js
+++ src/App.js
@@ -4,8 +4,6 @@ import './App.css';
 import {Route} from 'react-router-dom'
 import LoginSuccess from './login/LoginSuccess'
 import Login from './login/Login'
-var user_name_len =20;
-var user_pwd_len = 16;
 class App extends Component {
   render() {
     return (
@@ -18,8 +16,8 @@ class App extends Component {
         <p className="App-intro">
           逝者如斯，不舍昼夜。
         </p>
-          <Route path="/login" component={Login}> </Route>
-          <Route path="/login/success" component={LoginSuccess}/>
+          <Route exact='true' path="/login" component={Login}> </Route>
+          <Route exact= 'true' path="/login/success" component={LoginSuccess}/>
       </div>
     );
   }
