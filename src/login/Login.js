b1d40f8b9be6029cbe89f0a3f41717b65abed820
xujianhua <xujianhuaap@gmail.com>
2017-11-28 16:53:57 +0800

    【develop】login

--- src/login/Login.js
+++ src/login/Login.js
@@ -1,18 +1,40 @@
-import React,{ Component } from 'react';
+import React from 'react';
 import './login.css';
 const user_name_len =20;
 const user_pwd_len = 16;
-class Login extends Component{
+
+class Login extends React.Component{
+
+    constructor(props){
+        super(props);
+        this.state={user_name:'',user_pwd:''};
+
+        this.handleSubmit=this.handleSubmit.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(event){
+        console.log(event.target);
+        if (event.target.id === 'User-name'){
+            this.setState({user_name:event.target.value}) ;
+        }else if (event.target.id === 'User-password'){
+            this.setState({user_pwd:event.target.value}) ;
+        }
+
+    }
+    handleSubmit(){
+       alert(this.state.user_pwd);
+    }
 
      render(){
-        return  (<p className="App-login">
-                <input id="User-name" placeholder="用户名字"
-    maxLength={user_name_len} type="text"/>
-                <input id="User-password" placeholder="密码"
-    maxLength={user_pwd_len} type="password"/>
-                <input id="Submit" value="登陆" type="submit"/>
+        return  (<form className="App-login" onSubmit={this.handleSubmit} >
+                    <input id="User-name" placeholder="用户名字"
+                           maxLength={user_name_len} type="text" onChange={this.handleChange} />
+                    <input id="User-password" placeholder="密码"
+                           maxLength={user_pwd_len} type="password" onChange={this.handleChange} />
+                    <input id="Submit" value="登陆" type="submit" />
 
-        </p>);
+        </form>);
     }
 }
 
